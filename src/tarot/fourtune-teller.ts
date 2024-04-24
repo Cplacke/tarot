f63a67ba6d6e3f5b4b8f76ab210926241d0d85d1
Placke <cplacke@LAMU0Q10661GFDY.uhc.com>
2024-04-24 19:52:28 -0400

    shuffle seed

--- src/tarot/fourtune-teller.ts
+++ src/tarot/fourtune-teller.ts
@@ -1,9 +1,13 @@
 import { getTarotDeck } from './deck.ts';
+import shuffleSeed from 'npm:shuffle-seed';
 
 export const getReading = (count: number) => {
 
+    const seed = Date.now();
     const deck = getTarotDeck();
-    deck.sort(() => ((Math.random()*10)-5))
 
-    return deck.slice(0, count);
+    const shuffle1 = shuffleSeed.shuffle(deck, seed)
+    const shuffle2 = shuffleSeed.shuffle(shuffle1, seed)
+
+    return shuffle2.slice(0, count);
 }
