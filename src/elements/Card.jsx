65f5b6f0adce26ae9755fdafb5af2b67c62b7b41
Placke <cplacke@LAMU0Q10661GFDY.uhc.com>
2024-04-24 16:17:32 -0400

    remove on click to flip cards

--- src/elements/Card.jsx
+++ src/elements/Card.jsx
@@ -6,7 +6,6 @@ export const Card = (title, card) => {
         <h2 class="text-center"> ${title} </h2>
         <img 
             class="card d-block mx-auto" src="${card.image}"
-            onclick="reveal(event)"
         />
         <h3 class="text-center"> ${card.name} </h3>
         <p class="p-2">${card.interpretation}</p>
